fc8b02369a6ef1eefb85df59e473bae679159b86
Edimar Del Carmen Cordero Marte <edimar_cordero@ucne.edu.do>
2017-01-14 14:04:37 +0100

    Solo plantilla sin codigo

--- GitHub/Form1.Designer.cs
+++ GitHub/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.Insertarbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(94, 31);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombretextBox.TabIndex = 0;
+            // 
+            // Insertarbutton
+            // 
+            this.Insertarbutton.Location = new System.Drawing.Point(210, 31);
+            this.Insertarbutton.Name = "Insertarbutton";
+            this.Insertarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Insertarbutton.TabIndex = 1;
+            this.Insertarbutton.Text = "Insertar";
+            this.Insertarbutton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inserte Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 256);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Insertarbutton);
+            this.Controls.Add(this.NombretextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.Button Insertarbutton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
